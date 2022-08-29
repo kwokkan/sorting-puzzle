84d90dec4943f38a8906607ee37920fd1350412b
kwokkan <kwokkan@users.noreply.github.com>
2022-08-29 23:05:05 +0100

    Add PWA support.

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,10 @@ import { App } from "./App";
 
 var element = document.getElementById("app");
 render(<App />, element!);
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register(
+        new URL("sw.js", import.meta.url),
+        { type: "module" }
+    );
+}
