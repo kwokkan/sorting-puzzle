5d2ab3fa207a07c6b24cc3f1a170a97cfdddf283
kwokkan <kwokkan@users.noreply.github.com>
2022-09-20 22:43:47 +0100

    Add icon for undo.

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,13 +1,14 @@
-import CogSvg from "bundle-text:./assets/icons/cog.svg";
-import CrossSvg from "bundle-text:./assets/icons/cross.svg";
-import MinusSvg from "bundle-text:./assets/icons/minus.svg";
-import PlusSvg from "bundle-text:./assets/icons/plus.svg";
-import SpinnerSvg from "bundle-text:./assets/icons/spinner11.svg";
+import CloseSvg from "bundle-text:./assets/icons/Close.svg";
+import MinusSvg from "bundle-text:./assets/icons/Minus.svg";
+import PlusSvg from "bundle-text:./assets/icons/Plus.svg";
+import ResetSvg from "bundle-text:./assets/icons/Reset.svg";
+import SettingsSvg from "bundle-text:./assets/icons/Settings.svg";
+import UndoSvg from "bundle-text:./assets/icons/Undo.svg";
 import { JSXInternal } from "preact/src/jsx";
 
 export const CloseIcon = (props: JSXInternal.SVGAttributes): JSXInternal.Element => {
     return (
-        <div className="icon" dangerouslySetInnerHTML={{ __html: CrossSvg }}>
+        <div className="icon" dangerouslySetInnerHTML={{ __html: CloseSvg }}>
         </div>
     );
 };
@@ -28,14 +29,21 @@ export const PlusIcon = (props: JSXInternal.SVGAttributes): JSXInternal.Element 
 
 export const ResetIcon = (props: JSXInternal.SVGAttributes): JSXInternal.Element => {
     return (
-        <div className="icon" dangerouslySetInnerHTML={{ __html: SpinnerSvg }}>
+        <div className="icon" dangerouslySetInnerHTML={{ __html: ResetSvg }}>
         </div>
     );
 };
 
 export const SettingsIcon = (props: JSXInternal.SVGAttributes): JSXInternal.Element => {
     return (
-        <div className="icon" dangerouslySetInnerHTML={{ __html: CogSvg }}>
+        <div className="icon" dangerouslySetInnerHTML={{ __html: SettingsSvg }}>
+        </div>
+    );
+};
+
+export const UndoIcon = (props: JSXInternal.SVGAttributes): JSXInternal.Element => {
+    return (
+        <div className="icon" dangerouslySetInnerHTML={{ __html: UndoSvg }}>
         </div>
     );
 };
